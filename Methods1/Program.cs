3d5956c0579e5f94cd789740bd69552a355c6582
sport <sport@LAPTOP-61I2ACB0>
2023-09-15 22:44:11 +0300

    Add project files.

---
+++ Methods1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+int[] n = { 5, 7, 3, 2, 9 };
+int sum = SumArr(n);
+
+Console.WriteLine("Sum of all elements stored in the array is: " + sum);
+
+
+
+static int SumArr(int[] n)
+{
+    int sum = 0;
+    foreach (int item in n)
+    {
+        sum += item;
+    }
+    return sum;
+}
